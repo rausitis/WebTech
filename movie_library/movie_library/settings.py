25c5759d97a199cf372283bd26c2298f2f850635
paulaxinte <axinte.angelica123@gmail.com>
2024-10-28 22:10:30 +0100

    removed error causing line

--- movie_library/movie_library/settings.py
+++ movie_library/movie_library/settings.py
@@ -141,5 +141,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CORS_ALLOWED_ORIGINS = True
