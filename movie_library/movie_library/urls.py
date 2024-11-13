684b4dbd3755c6df2aa689b6eeb25a2d353e555f
apinto @ lardafada <senhoralexandrepinto@protonmail.com>
2024-11-13 17:11:17 +0100

    changes API route filename for clarity

--- movie_library/movie_library/urls.py
+++ movie_library/movie_library/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('movieApp.urls')),
+    path('api/', include('movieApp.api_routes')),
 ]
