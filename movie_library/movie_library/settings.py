a44837c4b0bc58b58d2598060c23351d93a0a074
rausitis <144431196+rausitis@users.noreply.github.com>
2024-12-17 21:54:13 +0100

    Ruta: Server change to RasPI

--- movie_library/movie_library/settings.py
+++ movie_library/movie_library/settings.py
@@ -92,13 +92,23 @@ WSGI_APPLICATION = 'movie_library.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'testdb1',
+        'USER': 'ruta',
+        'PASSWORD': 'ruta',
+        'HOST': 'pipi',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
